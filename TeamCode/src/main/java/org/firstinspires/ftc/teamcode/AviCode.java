9a05354b3c4c6815d315515a5569551243ecfb76
ultraTech010 <avi.bindlish@icloud.com>
2026-08-22 14:15:31 -0700

    added code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AviCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AviCode.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AviCode {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class AviCode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
+
+
