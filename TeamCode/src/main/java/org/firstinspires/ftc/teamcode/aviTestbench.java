b298c97bb0ecb3dbf04709265adc3dbd36f03ada
ultraTech010 <avi.bindlish@icloud.com>
2026-08-22 14:46:53 -0700

    added code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aviTestbench.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp
+public class aviTestbench extends OpMode {
+    private DcMotor testMotor;
+    private TouchSensor touchSensor;
+
+
+    @Override
+    public void init() {
+        testMotor  = hardwareMap.get(DcMotor.class, "testMotor");
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+    @Override
+    public void loop() {
+        if (touchSensor.isPressed()){
+            testMotor.setPower(0.2);
+        }
+
+        telemetry.addData("touch sensor status", touchSensor.isPressed());
+        telemetry.addData("motor power", testMotor.getPower());
+    }
+}
