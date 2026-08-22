7e5ad17f3e3cacf49ca7df25c35b2c244b7696e3
siddh <siddhbelur@gmail.com>
2026-08-22 15:17:34 -0700

    new code for motor

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/siddhtest.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/siddhtest.java
@@ -2,18 +2,34 @@ package org.firstinspires.ftc.robotcontroller;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp
 public class siddhtest extends OpMode {
 
+    private DcMotor testMotor = null;
+    private TouchSensor touchSensor;
+
     @Override
     public void init() {
+        testMotor  = hardwareMap.get(DcMotor.class, "testMotor");
+        touchSensor  = hardwareMap.get(TouchSensor.class, "touchSensor");
+        telemetry.addData("Status", "Initialized");
 
     }
 
     @Override
     public void loop() {
+        if (touchSensor.isPressed()){
+            testMotor.setPower(0.2);
+        }
+        if(!touchSensor.isPressed()){
+            testMotor.setPower(0);
+        }
 
+        telemetry.addData("touch sensor status", touchSensor.isPressed());
+        telemetry.addData("motor power", testMotor.getPower());
     }
 }
 
