65112c320c310912aeae49d8e1820f0cf0e31ff5
dr-owl-academy <dr.owl.academy@gmail.com>
2026-08-22 16:32:36 -0700

    added touch sensor counter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coachtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/coachtest.java
@@ -1,4 +1,61 @@
 package org.firstinspires.ftc.teamcode;
 
-public class coachtest {dfgjha
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+@TeleOp
+public class coachtest extends OpMode {
+    private DcMotor testMotor = null;
+    private TouchSensor touchSensor;
+
+    private int counter = 0;
+
+    private boolean wasPressed = false;
+
+
+    @Override
+    public void init() {
+        testMotor  = hardwareMap.get(DcMotor.class, "testMotor");
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        testMotor.setDirection(DcMotor.Direction.FORWARD);
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+    @Override
+    public void loop() {
+        /*
+        if (touchSensor.isPressed()){
+            testMotor.setPower(0.2);
+        }
+        if (!touchSensor.isPressed()){
+            testMotor.setPower(0);
+        }
+*/
+        counter = countTouchSensorPresses();
+
+        telemetry.addData("touch sensor status", touchSensor.isPressed());
+        telemetry.addData("motor power", testMotor.getPower());
+        telemetry.addData("number of presses", counter);
+    }
+
+    public int countTouchSensorPresses() {
+
+        boolean isPressed = touchSensor.isPressed();
+
+        // Count only when the sensor changes from
+        // not pressed -> pressed
+        if (isPressed && !wasPressed) {
+            counter++;
+
+        }
+        wasPressed = isPressed;
+
+
+        return counter;
+    }
 }
+
+
